450ef2255270a6797404afc496be6c629525d56c
Taro Nagasawa <taro.nagasawa@gmail.com>
2019-10-20 13:04:07 +0900

    不要なコードを削除

--- src/presentation/TaskController.kt
+++ src/presentation/TaskController.kt
@@ -6,15 +6,11 @@ import io.ktor.application.ApplicationCall
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
-import io.ktor.util.pipeline.PipelineContext
 import java.time.LocalDateTime
 
 class TaskController(
     private val taskRepository: TaskRepository
 ) {
-
-    fun index(pipelineContext: PipelineContext<Unit, ApplicationCall>) {
-    }
 
     suspend fun index(call: ApplicationCall) {
         val tasks = taskRepository.findTasks()
